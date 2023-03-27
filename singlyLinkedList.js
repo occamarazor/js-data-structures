76a953ab01bb97c477dd8169bba08b761bba25ce
Occama Razor <30173929+occamarazor@users.noreply.github.com>
2023-03-27 15:58:52 +0700

    Linked List: Singly Linked List findByValue

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -38,15 +38,20 @@ class SinglyLinkedList {
   #head = null;
   #tail = null; // Stored for efficiency
   #nodes = 0;
-
+  
+  getByIndex(i) {
+    // TODO: get an item by index
+    console.log(i);
+  }
+  
   insert(data) {
     const newNode = new Node(data);
-
+    
     // Reverse singly linked list
     // Wrong: in fact head is tail; next is previous
     // newNode.next = this.#head;
     // this.#head = newNode;
-
+    
     if(this.#nodes === 0) {
       this.#head = newNode;
       this.#tail = newNode;
@@ -54,36 +59,52 @@ class SinglyLinkedList {
       this.#tail.next = newNode;
       this.#tail = newNode;
     }
-
+    
     this.#nodes++;
   }
-
-  find(data) {
-    // TODO: find first item with given data
-    console.log(data);
-  }
-
-  delete(i) {
-    // TODO: delete item by index
+  
+  insertAtIndex(i) {
+    // TODO: insert item at index
     console.log(i);
   }
-
+  
+  findByValue(value) {
+    let currentNode = this.#head;
+    
+    while(currentNode !== null) {
+      if(currentNode.value === value) {
+        console.log(`Node with value "${value}" found`);
+        return;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    
+    console.log(`Node with value "${value}" not found`);
+  }
+  
+  deleteAtIndex(i) {
+    // TODO: delete item at index
+    console.log(i);
+  }
+  
   log() {
     console.log(`Head: ${this.#head?.value || this.#head}`);
     console.log(`Tail: ${this.#tail?.value || this.#tail}`);
     console.log(`Nodes: ${this.#nodes}`);
-
+    
     let currentNode = this.#head;
-
+    
     while(currentNode !== null) {
       console.log(`Node: ${currentNode.value}    Next: ${currentNode.next?.value || currentNode.next}`);
       currentNode = currentNode.next;
     }
-    console.log('-----------------------')
+    console.log('-----------------------');
   }
 }
 
 const singlyLinkedList = new SinglyLinkedList();
+// Insert
 singlyLinkedList.log();
 singlyLinkedList.insert(111);
 singlyLinkedList.log();
@@ -91,3 +112,6 @@ singlyLinkedList.insert(222);
 singlyLinkedList.log();
 singlyLinkedList.insert(333);
 singlyLinkedList.log();
+// Find
+singlyLinkedList.findByValue(123);
+singlyLinkedList.findByValue(333);
